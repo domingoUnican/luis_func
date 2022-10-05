59141c9582daf8e80d691094faddbb1c211806a2
Domingo Gómez <domingo.gomez@unican.es>
2022-10-05 17:43:22 +0200

    Finishing exploring with Branch and bound

--- codigo/representation.py
+++ codigo/representation.py
@@ -1,40 +1,53 @@
 # coding: utf-8
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from copy import deepcopy
 from collections import defaultdict
 
-@dataclass(frozen=True)
+
+@dataclass(frozen=True, order=True)
 class Node:
     id: int
     label: str
 
+    def __eq__(self, a):
+        return self.id == a.id
+
 
 @dataclass(frozen=True)
-class Common(Node): 
+class Common(Node):
     x: float
     y: float
     prc: int
 
-#This represents both requests and physical CU
+
+# This represents both requests and physical CU
 @dataclass(frozen=True)
 class CentralUnit(Common):
     type: int = 2
 
 
-#This represents both requests and physical DU
+# This represents both requests and physical DU
 @dataclass(frozen=True)
 class DistributedUnit(Common):
     prb: int
     ant: int
-    theta: int # If is a request, make this to 0
+    theta: int  # If is a request, make this to 0
     type: int = 1
 
 
 @dataclass(frozen=True)
 class Edge:
+
     source: int
     target: int
     bandwith: int
+
+    def __lt__(self, a):
+        return (self.source < a.source or
+                (self.source == a.source and self.target < a.target))
+
+    def __eq__(self, a):
+        return self.source == a.source and self.target == a.target
 
 
 @dataclass(frozen=True)
@@ -55,6 +68,8 @@ class Topology:
 
     def __init__(self, dictionary):
         self.dictionary = dictionary
+        self.dictionary["nodes"] = sorted(self.dictionary["nodes"])
+        self.dictionary["edges"] = sorted(self.dictionary["edges"])
 
     def get_nodes(self):
         return self.dictionary["nodes"]
@@ -64,27 +79,48 @@ class Topology:
 
     def get_node_by_id(self, id):
         for i in self.get_nodes():
-            if i.get_id() == id:
+            if i.id == id:
                 return i
 
-    def get_edge_by_id(self, id):
+    def get_edge_by_source(self, source):
         for i in self.get_edges():
-            if i.get_id() == id:
-                return i
+            if i.source == source:
+                yield i
+
+    def modify_edge_by_id(self, new_edge):
+        for pos, i in enumerate(self.get_edges()):
+            if i == new_edge:
+                break
+        else:
+            raise Exception("Edge not found")
+        self.dictionary["edges"][pos] = new_edge
+
+    def modify_node_by_id(self, new_node):
+        for pos, i in enumerate(self.get_edges()):
+            if i == new_node:
+                break
+        else:
+            raise Exception("Edge not found")
+        self.dictionary["nodes"][pos] = new_node
+
 
 class BranchAndBound:
     """Documentation for BranchAndBound
 
     """
+
     def __init__(self, physical, requests):
         self.node_correspondance = defaultdict(list)
         self.edge_correspondance = dict()
         self.original = deepcopy(physical)
-        self.physical = deepcopy(physical) # This is going to modify
+        self.physical = deepcopy(physical)  # This is going to be modified
         self.requests = deepcopy(requests)
+        self.initialize_connect()
+
+    def initialize_connect(self):
         self.connect = dict()
         self.size = dict()
-        for node in requests.get_nodes():
+        for node in self.requests.get_nodes():
             self.connect[node.id] = self.connect[node.id]
             self.size[node.id] = 1
 
@@ -116,15 +152,24 @@ class BranchAndBound:
 
     def first_link_unassigned(self):
         for link in self.requests.get_edges():
+            if link not in self.edge_correspondance:
+                return None
             path = self.edge_correspondance[link]
             a, b = link.source, link.target
-            a, b = self.node_correspondance[a], self.node_correspondance[b]
+            for temp1, temp2 in self.node_correspondance.items():
+                if temp1.id == a:
+                    a = temp2.id
+                    break
+            for temp1, temp2 in self.node_correspondance.items():
+                if temp1.id == b:
+                    b = temp2.id
+                    break
             if not path or path[0].source != a or path[-1].target != b:
                 return link
         return None
 
     def is_assigned(self):
-        if self.first_node_unassigned() != None or self.first_node_unassigned() != None:
+        if self.first_node_unassigned() is not None or self.first_node_unassigned() is not None:
             return False
         return True
 
@@ -139,6 +184,75 @@ class BranchAndBound:
         return len(Agk)/Pools
 
     def UoC(self):
-        pass
+        used_capacity = 0
+        num_links = 0
+        for link1, link2 in zip(sorted(self.original.get_edges()),
+                                sorted(self.physical.get_edges())):
+            num_links += 1
+            # The formula is the capacity minus the remainder
+            used_capacity += 1 - link2.bandwith/link1.bandwith
+        return used_capacity / num_links
+
     def cost(self):
         return self.DoC() * (1 - self.UoC())
+
+    def expand(self):
+        check_nodes_unassigned = False
+        for node in self.requests.get_nodes():
+            if all(node > i for i in self.node_correspondance):
+                continue
+            for target in self.physical.get_nodes():
+                aux = deepcopy(self)
+                if node.prc < target.prc:
+                    if isinstance(node, DistributedUnit) and isinstance(target, DistributedUnit):
+
+                        if (node.prb < target.prb) and (node.ant < target.ant):
+                            target_aux = DistributedUnit(target.id,
+                                                         target.label,
+                                                         target.x,
+                                                         target.y,
+                                                         target.prc - node.prc,
+                                                         target.prb - node.prb,
+                                                         target.ant - node.ant,
+                                                         target.theta)
+                            aux.physical.modify_node_by_id(target_aux)
+                            aux.node_correspondance[node] = target_aux
+                            yield aux
+                            check_nodes_unassigned = True
+                    elif isinstance(node, CentralUnit) and isinstance(target, CentralUnit):
+                        target_aux = CentralUnit(target.id,
+                                                 target.label,
+                                                 target.x,
+                                                 target.y,
+                                                 target.prc - node.prc)
+                        aux.physical.modify_node_by_id(target_aux)
+                        yield aux
+                        check_nodes_unassigned = True
+        if not check_nodes_unassigned:
+            link = self.first_link_unassigned()
+            initial, destiny = 0, 0
+            if link not in self.edge_correspondance:
+                self.initialize_connect()
+                a = link.source
+                for temp1, temp2 in self.node_correspondance.items():
+                    if temp1.id == a:
+                        a = temp2
+                        break
+                initial = a.source
+            else:
+                initial = self.edge_correspondance[link][-1].target
+            for temp in self.physical.get_edge_by_source(initial):
+                if (not self.is_connected(initial, temp.target) and link.bandwith < temp.bandwith):
+                    aux = deepcopy(self)
+                    temp1 = EdgePhysical(temp.source,
+                                         temp.target,
+                                         temp.bandwith - link.bandwith,
+                                         temp.distance,
+                                         temp.type)
+                    if link not in self.edge_correspondance:
+                        aux.edge_correspondance[link] = [temp1]
+                    else:
+                        aux.edge_correspondance[link] += [temp1]
+                    aux.join(initial, destiny)
+                    aux.physical.modify_edge_by_id(temp1)
+                    yield aux
